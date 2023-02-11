753399b0e1102057a1870f5ddb1415bd3f756e24
Gabriel Seleguin Rodrigues de Brito <90803563+Gabriel2021GSRB@users.noreply.github.com>
2023-02-11 19:09:33 -0300

    criação do arquivo auto escola.

---
+++ auto_escola.py
@@ -0,0 +1,9 @@
+idade = 0
+input("digite seu nome:")
+input("digite sua idade:")
+input("você deseja tirar sua carteira de abilitação?")
+input("Você ja fez o exame de vista?")
+input("Você ja fez o exame psicotécnico:") 
+input("Você ja fez o cursinho teorico?")
+input("Você ja fez as aulas praticas?")
+print("Parabens sua carteira de motorista chega em duas semanas!!!")
